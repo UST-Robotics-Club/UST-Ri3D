d1a4ef35a69a008285cef4a192f91b217040b34e
Ag3nt-9 <85025522+Ag3nt-9@users.noreply.github.com>
2025-01-05 16:34:50 -0600

    Outtake Now Exists

--- UST-Ri3D/src/main/java/frc/robot/subsystems/Intake.java
+++ UST-Ri3D/src/main/java/frc/robot/subsystems/Intake.java
@@ -8,12 +8,10 @@ import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.IntakeConstants;
 
 public class Intake extends SubsystemBase {

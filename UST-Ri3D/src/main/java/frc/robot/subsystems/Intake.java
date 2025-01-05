de7a775a802b8f1424a58a04140506a0f21857de
LukeBorowy <lukeborowy03@gmail.com>
2025-01-05 09:51:42 -0600

    Intake and constants for drivetrain

--- UST-Ri3D/src/main/java/frc/robot/subsystems/Intake.java
+++ UST-Ri3D/src/main/java/frc/robot/subsystems/Intake.java
@@ -4,14 +4,76 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.IntakeConstants;
 
 public class Intake extends SubsystemBase {
-  public Intake() {}
+  
 
+  private SparkMax deployer = new SparkMax(IntakeConstants.deployerId, MotorType.kBrushless);
+  private SparkMax intakeMotor = new SparkMax(IntakeConstants.intakeMotorId, MotorType.kBrushless);
+  private SparkMax internalReleaser = new SparkMax(IntakeConstants.internalReleaserId, MotorType.kBrushless);
+  private long timeToStopRelease = 0;
+
+  public Intake() {
+    SparkMaxConfig config = new SparkMaxConfig();
+    config.idleMode(IdleMode.kBrake);
+    deployer.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
+    intakeMotor.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
+    internalReleaser.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
+  }
   @Override
   public void periodic() {
+    if(timeToStopRelease <= System.currentTimeMillis()) {
+      internalReleaser.set(IntakeConstants.releaserSpeed);
+    } else {
+      internalReleaser.set(0);
+    }
     
+    SmartDashboard.putNumber("deployerPos", 
+    deployer.getEncoder().getPosition() * DriveConstants.rotationConversionFactor);
+
+    SmartDashboard.putNumber("intakeSpeed", 
+    intakeMotor.getEncoder().getVelocity());
+
+    SmartDashboard.putNumber("releaseSpeed", 
+    internalReleaser.getEncoder().getVelocity());
+  }
+  // Move intake to deployed position
+  public void deploy() {
+    deployer.getClosedLoopController().setReference(IntakeConstants.deployedPosition, ControlType.kPosition);
+  }
+  // Move intake to retracted position
+  public void retract() {
+    deployer.getClosedLoopController().setReference(IntakeConstants.retractedPosition, ControlType.kPosition);
+  }
+  // start intaking
+  public void setIntakeOn() {
+    intakeMotor.set(IntakeConstants.intakeSpeed);
   }
 
+  // Stop intaking
+  public void setIntakeOff() {
+    intakeMotor.set(0);
+  }
+
+  // Run the release motor for a default time
+  public void releaseCoral() {
+    timeToStopRelease = System.currentTimeMillis() + IntakeConstants.releaseTime;
+  }
+
+  // Run the release motor for a custom time
+  public void releaseCoral(int time) {
+    timeToStopRelease = System.currentTimeMillis() + time;
+  }
 }

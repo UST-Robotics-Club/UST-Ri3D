5c56aa8e6466aead2b5fe6bc8bf1e1aaad8bd006
Lukas Singer <56053273+LukasSinger@users.noreply.github.com>
2025-01-05 10:11:43 -0600

    Start elevator

--- UST-Ri3D/src/main/java/frc/robot/subsystems/Elevator.java
+++ UST-Ri3D/src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,14 +4,34 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkMax;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ElevatorConstants;
 
 public class Elevator extends SubsystemBase {
-  public Elevator() {}
+
+  SparkMax motor = new SparkMax(ElevatorConstants.elevatorID, MotorType.kBrushless);
+
+  public Elevator() {
+    SparkMaxConfig config = new SparkMaxConfig();
+    config.idleMode(IdleMode.kBrake);
+    config.closedLoop
+      .p(ElevatorConstants.p)
+      .i(ElevatorConstants.i)
+      .d(ElevatorConstants.d)
+      .outputRange(ElevatorConstants.minOutput, ElevatorConstants.maxOutput);
+    motor.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
+  }
 
   @Override
   public void periodic() {
-    
   }
+
+  
 
 }
